d62dc39120f95b344abd2998b809623538b974c1
Lucas Sileo <lucastoledo358@gmail.com>
2023-07-18 17:09:49 -0300

    deploy yml

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-   plugins: [react()],
-   base: '/Portfolio/',
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/Portfolio',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/Portfolio/'
+  }
+
+  return config
 })
