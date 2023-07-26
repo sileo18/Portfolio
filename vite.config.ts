2886ec6031a73c64b5d790a9f8daec0c9f29dfc3
Lucas Sileo <lucastoledo358@gmail.com>
2023-07-26 00:12:20 -0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
-    base: '/Portfolio',
+    base: '/Portfolio/',
   }
 
   if (command !== 'serve') {
